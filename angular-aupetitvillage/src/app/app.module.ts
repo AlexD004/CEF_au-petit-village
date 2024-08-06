2166c4c63b308fecae460d099cb7de555c9e820d
AlexD004 <dupre.alexandre04@gmail.com>
2024-08-06 14:48:32 +0200

    add basic components

--- angular-aupetitvillage/src/app/app.module.ts
+++ angular-aupetitvillage/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageAboutComponent } from './page-about/page-about.component';
+import { PageContactComponent } from './page-contact/page-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageAboutComponent,
+    PageContactComponent
   ],
   imports: [
     BrowserModule,
